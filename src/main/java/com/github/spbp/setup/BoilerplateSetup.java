ec1921c688f6c201c98292287e5d84801819beca
boformer <felix.free@gmx.de>
2015-03-11 22:26:45 +0100

    Add build.gradle, main class

---
+++ src/main/java/com/github/spbp/setup/BoilerplateSetup.java
@@ -0,0 +1,9 @@
+package com.github.spbp.setup;
+
+public class BoilerplateSetup {
+
+	public static void main(String[] args) {
+		//TODO
+		//Use wizards: http://www.oracle.com/technetwork/articles/javase/wizard-136789.html
+	}
+}
